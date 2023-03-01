1faad57dc9a9f98a79babe99c525611e72da115f
G1320 <darnellgreen211@gmail.com>
2023-03-01 12:10:02 +0200

    Added keep HTML skeleton

--- views/KeepIndex.js
+++ views/KeepIndex.js
@@ -1,16 +1,16 @@
 export default {
   template: `
-  <header class="main-keep-nav">
-  <p>I AM A NAV HAMBURGER</p>
-  <p>I AM A NAV LOGO</p>
-  <p>I AM A NAV INPUT</p>
-  <p>I AM A NAV ITEM</p>
-  <p>I AM A NAV ITEM</p>
-  <p>I AM A NAV ITEM</p>
-  <p>I AM A NAV ITEM</p>
+  <header class="keep-main-nav">
+  <p> HAMBURGER</p>
+  <p> LOGO</p>
+  <p> INPUT</p>
+  <p> ITEM</p>
+  <p> ITEM</p>
+  <p> ITEM</p>
+  <p> ITEM</p>
   </header>
 
-<div class="add-note">
+<div class="keep-add-note">
   
   <P>
     i AM Component add note
@@ -19,18 +19,19 @@ export default {
 
 
 
-  <main class="main-keep-content"></main>
+  <main class="keep-main-keep-content">
 
-    <div class="filter-list">
+    
+    <div class="keep-filter-list">
       <p>I AM A NOTE</p>
       <p>I AM A REMINDER</p>
       <p>I AM A EDIT LABEL</p>
       <p>I AM A ARCHIVE</p>
       <p>I AM A TRASH</p>
-
-<div/>
-
-    <div class="note-list">
+      
+    </div>
+    
+    <div class="keep-note-list">
       <p>I AM A NOTE</p>
       <p>I AM A NOTE</p>
       <p>I AM A NOTE</p>
@@ -38,6 +39,7 @@ export default {
       <p>I AM A NOTE</p>
     </div>
     
+  </main>
 
 
     `,
